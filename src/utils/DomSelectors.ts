8f48573cf22fc729309a8641fcd181efea59dfff
NGOCDG280\ngocdg <pnd280@gmail.com>
2024-09-13 10:19:28 +0700

    fix(css): update sticky navbar selector

--- src/utils/DomSelectors.ts
+++ src/utils/DomSelectors.ts
@@ -63,7 +63,7 @@ const DomSelectors = {
     WRAPPER: ".grow.block",
     PRO_SEARCH_TOGGLE: "button#copilot-toggle",
   },
-  STICKY_NAVBAR: ".sticky.left-0.right-0.top-0.z-20.border-b",
+  STICKY_NAVBAR: ".sticky.left-0.right-0.top-0.border-b",
 } as const;
 
 const DomHelperSelectors = {
