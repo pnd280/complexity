0506ad2781dd3343cf3c4e79a39ef8d22b70ea1b
Pham Ngoc Duong <pnd280@gmail.com>
2025-10-24 07:12:29 +0700

    chore: update deps

--- perplexity/extension/src/plugins/language-model-selector/components/mobile/index.tsx
+++ perplexity/extension/src/plugins/language-model-selector/components/mobile/index.tsx
@@ -2,29 +2,19 @@ import type { DialogProps } from "@/components/ui/dialog";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 import { usePluginGuardsStore } from "@/plugins/__async-deps__/plugins-guard/store";
 import LanguageModelGroup from "@/plugins/language-model-selector/components/mobile/LanguageModelGroup";
-import { getAdvancedStandaloneModels } from "@/plugins/language-model-selector/utils";
-import { PplxLanguageModelsService } from "@/services/externals/cplx-api/remote-resources/pplx-language-models";
+import {
+  getAdvancedStandaloneModels,
+  getModelsByType,
+} from "@/plugins/language-model-selector/utils";
 import { LanguageModelTypeIcons } from "@/services/externals/cplx-api/remote-resources/pplx-language-models/icons";
 
 export default function MobileContent({ ...props }: DialogProps) {
   const subTier = usePluginGuardsStore((store) => store.subTier);
 
-  const searchModels = useMemo(
-    () => PplxLanguageModelsService.allModels.search,
-    [],
-  );
-  const researchModels = useMemo(
-    () => PplxLanguageModelsService.allModels.research,
-    [],
-  );
-  const labsModels = useMemo(
-    () => PplxLanguageModelsService.allModels.studio,
-    [],
-  );
-  const studyModels = useMemo(
-    () => PplxLanguageModelsService.allModels.study,
-    [],
-  );
+  const searchModels = useMemo(() => getModelsByType("search"), []);
+  const researchModels = useMemo(() => getModelsByType("research"), []);
+  const labsModels = useMemo(() => getModelsByType("studio"), []);
+  const studyModels = useMemo(() => getModelsByType("study"), []);
   const advancedModels = useMemo(() => getAdvancedStandaloneModels(), []);
 
   return (
